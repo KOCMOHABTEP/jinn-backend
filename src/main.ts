8b732a19f41e4a7e9d116f8a59075321c83038f8
Dmitriy Smirnov <dsmirnov.online@yandex.ru>
2025-03-17 19:04:37 +0300

    feat: cors tmp fix

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: 'http://localhost:3000',
-    credentials: true,
+    allowedHeaders: '*',
+    origin: '*',
   });
+
   await app.listen(5000);
 }
 bootstrap();
